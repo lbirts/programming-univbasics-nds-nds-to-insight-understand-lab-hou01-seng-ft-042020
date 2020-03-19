cb42d98f644fa6dab1b682c95bd6fb8c51d33436
Lauren Birts <birtslauren@gmail.com>
2020-03-19 18:50:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,6 @@ end
 def print_first_directors_movie_titles
   row_index = 0
   movies = directors_database[0][:movies]
-    while row_index < movies.count do
+    while row_index < movies.length do
       puts movies[element_numb]
 end
